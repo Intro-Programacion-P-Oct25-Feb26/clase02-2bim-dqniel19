8e7704cf81835fc1b0e19fc3f3b38dff61649fe1
Daniel <danielp4p1@gmail.com>
2025-12-10 12:08:45 -0500

    commit

--- Ejemplos/src/paquete1/Ejemplo042.java
+++ Ejemplos/src/paquete1/Ejemplo042.java
@@ -9,14 +9,14 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo041 {
+public class Ejemplo042 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         // int[] arreglo1 = new int[5];
         int suma = 0;
@@ -25,16 +25,19 @@ public class Ejemplo041 {
         // System.out.println(arreglo1.length);
         // arreglo1[2] = 1000;
         for (int i = 0; i < arreglo1.length; i++) {
-            suma = suma + arreglo1[i];
-            acumulador = String.format("%sSubíndice o Índice %d, "
-                    + "con valor %d\n", 
-                    acumulador, i, arreglo1[i]);
+            if (arreglo1[i] >= 1000) {
+                suma = suma + arreglo1[i];
+                acumulador = String.format("%sSubíndice o Índice %d, "
+                        + "con valor %d\n",
+                        acumulador, i, arreglo1[i]);
+            }
+
         }
-        acumulador = String.format("%sLa suma total es: %d\n", 
+        acumulador = String.format("%sLa suma total es: %d\n",
                 acumulador, suma);
-        System.out.printf("%s\n", 
+        System.out.printf("%s\n",
                 acumulador);
-        
+
     }
 
 }
